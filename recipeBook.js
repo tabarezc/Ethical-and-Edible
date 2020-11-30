a85b5b372101bfe6a4d112f3de11b25d52e68dc1
Natalie Haness <hanessn@oregonstate.edu>
2020-11-29 19:53:05 -0800

    Add recipe modal

--- recipeBook.js
+++ recipeBook.js
@@ -1,3 +1,6 @@
+window.addEventListener('DOMContentLoaded', function(event) {
+    setupModal();
+});
 function loadRecipes(){
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function() {
@@ -10,4 +13,30 @@ function loadRecipes(){
     }
     xmlhttp.open("GET", "recipes.json", true);
     xmlhttp.send();
+}
+function setupModal(){
+    var modal = document.getElementById("myModal");
+
+    // Get the button that opens the modal
+    var btn = document.getElementById("addRecipeButton");
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks on the button, open the modal
+    btn.onclick = function() {
+      modal.style.display = "block";
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+      modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    }
 }
